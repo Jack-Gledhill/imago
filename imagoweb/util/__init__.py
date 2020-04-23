74ee01f486e5a8f0b35c8c9512bf9ea9ce77141f
Jack-Gledhill <jackgledhill2005@gmail.com>
2020-04-23 21:52:23 +0100

    Fix custos missing errors that aren't helpful

--- imagoweb/util/__init__.py
+++ imagoweb/util/__init__.py
@@ -7,22 +7,11 @@
 from os import _exit
 from datetime import datetime
 
-# ------------------------
-# Third-party dependencies
-# ------------------------
-try:
-    from custos import Custos
+# -------------------------
+# Local extension libraries
+# -------------------------
+from imagoweb.custos import Custos
 
-except ImportError:
-    now = datetime.utcnow()
-    
-    print(f"{now.strftime('%d-%m-%Y')} {now.strftime(format='%H:%M:%S')}:{str(round(number=int(now.strftime(format='%f'))/1000)).rjust(3, '0')} \x1b[31mFATAL\x1b[0m  [\x1b[32mimago:\x1b[0m\x1b[34mutil.init\x1b[0m]: custos dependency is missing.")
-
-    _exit(status=2)
-
-# --------------------------
-# Import extension libraries
-# --------------------------
 from .constants import *
 
 console = Custos(project_name="imago",
